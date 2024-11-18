e9ba997ea934957e85ad8f0625e80a5394739544
aaurÃelie sinanian <aurelie.sinanian@gmail.com>
2024-11-18 16:18:35 +0100

    update navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Buddie.create(friend_name: "Gordon Bleu",friend_mail: "gordonbleu@gmail.com", skill: "Cuisinier", description: "Frétilleur de Papilles", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.menshealth.com%2Fuk%2Ffitness%2Fa62043427%2Fgordon-ramsay-mens-health%2F&psig=AOvVaw1eTWrCvowGCdl3QPJQAx3i&ust=1732025932238000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID-ysiJ5okDFQAAAAAdAAAAABAE", rating: 7, address: "Buckingham Palace", price: "230$/hour", avalaible: true)
+
+Buddie.create(friend_name: "Amélie Lens",friend_mail: "amelielens@gmail.com", skill: "Dj", description: "Je vais te faire bouger le popotin comme personnee", image: "https://i.ytimg.com/vi/oCbUZ9X5Gkw/hqdefault.jpg?v=64d2567c", rating: 9, address: "Tomorrow Land", price: "700$/hour", avalaible: true)
+
+Buddie.create(friend_name: "Ken Sanbarbie",friend_mail: "ken@gmail.com", skill: "Beau Gosse", description: "Par mon physique avantageux je vous met en lumière le temps de la location", image: "https://images.mattel.net/image/upload/c_scale,f_a…xcbmw63q_d277f418-345e-49d9-bb64-6c4140656262.jpg", rating: 8, address: "Barbieland", price: "150$/hour", avalaible: true)
+
+Buddie.create(friend_name: "Joe",friend_mail: "joelerigolo@gmail.com", skill: "Clown", description: "j'anime les soirées comme personne, fin blagueur rigolade assurée", image: "https://www.le-geant-de-la-fete.com/26127-large_default/masque-de-visage-clown-tueur.jpg", rating: 8, address: "Cirque Medrano", price: "100$/hour", avalaible: true)
+
+Buddie.create(friend_name: "Sam ",friend_mail: "jesuistonsam@gmail.com", skill: "Taxi", description: "Frétilleur de Papilles", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.menshealth.com%2Fuk%2Ffitness%2Fa62043427%2Fgordon-ramsay-mens-health%2F&psig=AOvVaw1eTWrCvowGCdl3QPJQAx3i&ust=1732025932238000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID-ysiJ5okDFQAAAAAdAAAAABAE", rating: 7, address: "Buckingham Palace", price: "230$/hour", avalaible: true)
+
+Buddie.create(friend_name: "Gordon Bleu",friend_mail: "gordonbleu@gmail.com", skill: "Cuisinier", description: "Frétilleur de Papilles", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.menshealth.com%2Fuk%2Ffitness%2Fa62043427%2Fgordon-ramsay-mens-health%2F&psig=AOvVaw1eTWrCvowGCdl3QPJQAx3i&ust=1732025932238000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID-ysiJ5okDFQAAAAAdAAAAABAE", rating: 7, address: "Buckingham Palace", price: "230$/hour", avalaible: true)
+
+Buddie.create(friend_name: "Gordon Bleu",friend_mail: "gordonbleu@gmail.com", skill: "Cuisinier", description: "Frétilleur de Papilles", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.menshealth.com%2Fuk%2Ffitness%2Fa62043427%2Fgordon-ramsay-mens-health%2F&psig=AOvVaw1eTWrCvowGCdl3QPJQAx3i&ust=1732025932238000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID-ysiJ5okDFQAAAAAdAAAAABAE", rating: 7, address: "Buckingham Palace", price: "230$/hour", avalaible: true)
+
+Buddie.create(friend_name: "Gordon Bleu",friend_mail: "gordonbleu@gmail.com", skill: "Cuisinier", description: "Frétilleur de Papilles", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.menshealth.com%2Fuk%2Ffitness%2Fa62043427%2Fgordon-ramsay-mens-health%2F&psig=AOvVaw1eTWrCvowGCdl3QPJQAx3i&ust=1732025932238000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID-ysiJ5okDFQAAAAAdAAAAABAE", rating: 7, address: "Buckingham Palace", price: "230$/hour", avalaible: true)
