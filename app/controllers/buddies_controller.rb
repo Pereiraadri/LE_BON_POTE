ce3938fdd4a00940a5486bbd3890140cddb48639
Sami Chabane <chabanesami7@gmail.com>
2024-11-19 13:29:04 +0100

    buddies controller

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,10 +1,24 @@
 class BuddiesController < ApplicationController
   def new
+      @buddy = buddy.new
   end
 
   def create
+    @buddy = Buddy.new(buddy_params)
+    if @buddy.save
+      redirect_to @buddy, notice: 'Buddy was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @buddy = Buddy.find(params[:id])
+  end
+
+  private
+
+  def buddy_params
+    params.require(:buddy).permit(:name, :email, :phone, :other_attributes)
   end
 end
