2d59f33a9847a5235123af7a960cb710c2d9b593
adrien pereira <pereiraadrien3@gmail.com>
2024-11-19 12:48:10 +0100

    controller-view-routes-booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,23 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+
   end
 
   def show
-    @booking = Booking.find(params[:id])
+
   end
 
   def new
-    @booking = Booking.new
+
   end
 
   def create
-    @booking = Booking.new(params_booking)
-    if
   end
 
   private
 
   def params_booking
-    params.require(:booking).permit(:address, :start_date, :end_date)
+
   end
 
 
