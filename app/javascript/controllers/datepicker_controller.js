6a28ecc2380d15b19677cd702ea1eca57dcb907a
adrien pereira <pereiraadrien3@gmail.com>
2024-11-22 13:52:30 +0100

    fix bug alert

--- app/javascript/controllers/datepicker_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-import flatpickr from "Flatpickr";
-
-// Connects to data-controller="datepicker"
-export default class extends Controller {
-  connect() {
-    flatpickr(this.element)
-  }
-}
